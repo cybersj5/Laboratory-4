7a7daa0ac426f9327c0da398a348cb136628a3ae
cybersj5 <163285309+cybersj5@users.noreply.github.com>
2024-11-25 19:18:34 +0700

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Shared;
+using View;
+
+namespace lab
+{
+    public partial class Form1 : Form, IView
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public event Action<EventArgs> AddDataEvent;
+        public event Action<int> DeleteDataEvent;
+        public void RedrawForm(IEnumerable<EventArgs> data)
+        {
+            listView1.Items.Clear();
+            foreach (StudentEventArgs item in data)
+            {
+                ListViewItem listViewItem = new ListViewItem(item.Name);
+                listViewItem.SubItems.Add(item.Speciality);
+                listViewItem.SubItems.Add(item.Group);
+                listView1.Items.Add(listViewItem);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DeleteDataEvent?.Invoke(listView1.SelectedIndices[0]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AddDataEvent?.Invoke(new StudentEventArgs()
+            {
+                Name = textBox1.Text,
+                Speciality = textBox2.Text,
+                Group = textBox3.Text,
+            });
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Lab3.4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab3.4")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a2f7fd93-4acb-44f0-8d3a-bffa90fd6631")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
